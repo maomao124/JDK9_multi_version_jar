4b0379fe1d01a28ffb99fa2db2d758b9d66cf21f
mao <1296193245@qq.com>
2023-10-31 11:22:47 +0800

    JDK9_multi_version_jar

---
+++ src/main/java/mao/jdk17/Test.java
@@ -0,0 +1,11 @@
+package mao.jdk17;
+
+
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("jdk17");
+    }
+}
---
+++ src/main/java/mao/jdk8/Test.java
@@ -0,0 +1,11 @@
+package mao.jdk8;
+
+
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("jdk8");
+    }
+}
